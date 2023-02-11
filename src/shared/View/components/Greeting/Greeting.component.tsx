7fa2266a2bd52be5c71b573108e724520476ce0f
Alexandre Wotawa <awotawa@gmail.com>
2023-02-11 18:33:52 +0100

    feat(greeting): create greeting component

---
+++ src/shared/View/components/Greeting/Greeting.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {GreetingText, GreetingView} from './Greeting.styles';
+
+export const Greeting = () => {
+  return (
+    <GreetingView>
+      <GreetingText>Hello</GreetingText>
+    </GreetingView>
+  );
+};
